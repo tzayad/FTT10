4502902d88016edc225f86a0ea139c74ccaca715
tzayad <91532387+tzayad@users.noreply.github.com>
2023-02-17 22:38:46 +0200

    Update __init__.py

--- ftt10/__init__.py
+++ ftt10/__init__.py
@@ -1,1 +1,10 @@
-
+from dataclasses import dataclass
+import sys, smtplib, json, urllib3
+import matplotlib.pyplot as plt
+from datetime import datetime
+from pytrends.request import TrendReq
+import numpy as np
+import pandas as pd
+import yfinance as yf
+import tqdm.notebook as tq
+urllib3.disable_warnings()
